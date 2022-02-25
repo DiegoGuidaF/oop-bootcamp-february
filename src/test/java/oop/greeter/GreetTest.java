6a125abef21c12338c8e48e6fbe16b4043e039f4
Benjamin Tarenne <b.tarenne@thoughtworks.com>
2022-02-25 11:51:32 +0100

    Fix tab size

--- src/test/java/oop/greeter/GreetTest.java
+++ src/test/java/oop/greeter/GreetTest.java
@@ -2,6 +2,7 @@ package oop.greeter;
 
 import java.time.Instant;
 import java.time.ZoneId;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -13,37 +14,36 @@ import static org.testng.Assert.assertEquals;
 
 public class GreetTest {
 
-  String morningInstant = "2022-02-25T10:15:30Z";
-  String afternoonInstant = "2022-02-25T15:15:30Z";
-  Clock clock = Clock.fixed(Instant.parse(morningInstant), ZoneId.of("UTC"));
-  Greet greet = new Greet(clock);
-
-  @Test
-  public void itShouldReturnHelloNameInTheAfternoon() {
-    Clock clock = Clock.fixed(Instant.parse(afternoonInstant), ZoneId.of("UTC"));
+    String morningInstant = "2022-02-25T10:15:30Z";
+    String afternoonInstant = "2022-02-25T15:15:30Z";
+    Clock clock = Clock.fixed(Instant.parse(morningInstant), ZoneId.of("UTC"));
     Greet greet = new Greet(clock);
-    assertEquals(greet.greet("Chuck Norris"), "Hello Chuck Norris");
-  }
 
-  @Test
-  public void itShouldTrimInputAndReturnNameWhenGreet() {
-    assertEquals(greet.greet("Chuck Norris "), "Good morning Chuck Norris");
-  }
+    @Test
+    public void itShouldReturnHelloNameInTheAfternoon() {
+        Clock clock = Clock.fixed(Instant.parse(afternoonInstant), ZoneId.of("UTC"));
+        Greet greet = new Greet(clock);
+        assertEquals(greet.greet("Chuck Norris"), "Hello Chuck Norris");
+    }
 
-  @Test
-  public void itShouldCapitalizeFirstLetterOfName() {
-    assertEquals(greet.greet("chuck Norris"), "Good morning Chuck Norris");
-  }
+    @Test
+    public void itShouldTrimInputAndReturnNameWhenGreet() {
+        assertEquals(greet.greet("Chuck Norris "), "Good morning Chuck Norris");
+    }
 
-  @Test
-  public void itShouldGreetMorningDuringMorningTime() {
-    assertEquals(greet.greet("Chuck Norris"), "Good morning Chuck Norris");
-  }
+    @Test
+    public void itShouldCapitalizeFirstLetterOfName() {
+        assertEquals(greet.greet("chuck Norris"), "Good morning Chuck Norris");
+    }
 
-  @Test
-  public void itShouldGreetGoodEveningDuringEveningTime() {
+    @Test
+    public void itShouldGreetMorningDuringMorningTime() {
+        assertEquals(greet.greet("Chuck Norris"), "Good morning Chuck Norris");
+    }
+
+    @Test
+    public void itShouldGreetGoodEveningDuringEveningTime() {
 
 
-
-  }
+    }
 }
