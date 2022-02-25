b4fbc8f9ebba7d69a11847fb004862f74c06042b
Irene Torres <irene.t@thoughtworks.com>
2022-02-25 12:42:31 +0100

    Refactor method to simplify implementation

--- src/main/java/oop/stringCalculator/StringCalculator.java
+++ src/main/java/oop/stringCalculator/StringCalculator.java
@@ -9,13 +9,12 @@ public class StringCalculator {
     public int calculate(String stringToCalculate) {
         if (stringToCalculate.isEmpty()) {
             return 0;
-        } else if (stringToCalculate.contains(",")) {
-            List<Integer> integerList = Arrays.stream(stringToCalculate.split(",")).map(
-                Integer::parseInt).collect(
-                Collectors.toList());
-            return integerList.stream()
-                .reduce(0, Integer::sum);
         }
-        return Integer.parseInt(stringToCalculate);
+        List<Integer> integerList = Arrays.stream(stringToCalculate.split(",")).map(
+            Integer::parseInt).collect(
+            Collectors.toList());
+        return integerList.stream()
+            .reduce(0, Integer::sum);
+
     }
 }
