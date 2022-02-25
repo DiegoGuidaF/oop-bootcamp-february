e8dbc11f25add3c82db52b1b2783f756704e8aa3
Irene Torres <irene.t@thoughtworks.com>
2022-02-25 11:19:48 +0100

    Add morning greeting and refactor tests

--- src/test/java/oop/greeter/GreetTest.java
+++ src/test/java/oop/greeter/GreetTest.java
@@ -1,35 +1,45 @@
 package oop.greeter;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Clock;
 
+import static org.mockito.Mockito.after;
+import static org.mockito.Mockito.mock;
 import static org.testng.Assert.assertEquals;
 
 public class GreetTest {
-    Clock clock = Clock.systemDefaultZone();
 
+  String morningInstant = "2022-02-25T10:15:30Z";
+  String afternoonInstant = "2022-02-25T15:15:30Z";
+  Clock clock = Clock.fixed(Instant.parse(morningInstant), ZoneId.of("UTC"));
+  Greet greet = new Greet(clock);
+
+  @Test
+  public void itShouldReturnHelloNameInTheAfternoon() {
+    Clock clock = Clock.fixed(Instant.parse(afternoonInstant), ZoneId.of("UTC"));
     Greet greet = new Greet(clock);
+    assertEquals(greet.greet("Chuck Norris"), "Hello Chuck Norris");
+  }
 
-    @Test
-    public void itShouldReturnHelloName() {
-        assertEquals(greet.greet("Chuck Norris"), "Hello Chuck Norris");
-    }
+  @Test
+  public void itShouldTrimInputAndReturnNameWhenGreet() {
+    assertEquals(greet.greet("Chuck Norris "), "Good morning Chuck Norris");
+  }
 
-    @Test
-    public void itShouldTrimInputAndReturnNameWhenGreet() {
-        assertEquals(greet.greet("Chuck Norris "), "Hello Chuck Norris");
-    }
+  @Test
+  public void itShouldCapitalizeFirstLetterOfName() {
+    assertEquals(greet.greet("chuck Norris "), "Good morning Chuck Norris");
+  }
 
-    @Test
-    public void itShouldCapitalizeFirstLetterOfName() {
-        assertEquals(greet.greet("chuck Norris "), "Hello Chuck Norris");
-    }
+  @Test
+  public void itShouldGreetMorningDuringMorningTime() {
+    assertEquals(greet.greet("Chuck Norris"), "Good morning Chuck Norris");
+  }
 
-    //TODO: Fake time of clock in order to place it in the morning
-    //@Test
-    //public void itShouldGreetMorningDuringMorningTime() {
-    //    assertEquals(greet.greet("Chuck Norris"), "Good morning Chuck Norris");
-    //}
+
 
 }
